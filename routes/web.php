90ad141f70fa7bbf50592b2a6cb18199de0d21fd
ArturM2005 <arturmuhamediarov@gmail.com>
2025-05-07 16:07:48 +0300

    Added latin tags

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,25 @@ Route::get('/tag/{tag}', function (string $tag) {
     switch ($tag) {
         case 'политика':
             return 'новости политики';
+        case 'politics':
+            return 'новости политики';
         case 'спорт':
             return 'новости спорта';
+        case 'sport':
+            return 'новости спорта';
         case 'авто':
+            return 'новости авто';
+        case 'auto':
             return 'новости авто';
     };
 });
 Route::get('nauka', function () {
     return 'новости о науке';
 });
-
+Route::redirect('/news.html/{name?}', '/', 301);
+Route::redirect('/nauka.html', '/nauka', 301);
+Route::redirect('/nauka/11-10-1999_nobel.htm', '/nauka', 301);
+Route::redirect('/наука', '/nauka', 301);
+Route::redirect('/tag/здоровье', '/medicina', 301);
+Route::redirect('/tag/hi-tech', '/hitech', 301);
+Route::redirect('/internet', '/hitech', 301);
