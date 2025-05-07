6a717e4bbe163ddd57b9123f3f5a4467ec6ab88c
ArturM2005 <arturmuhamediarov@gmail.com>
2025-05-07 09:28:45 +0300

    Added new routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hitech', function () {
+    return 'Новости о высоких технологиях';
+});
+Route::get('/medicina', function () {
+    return 'Новости о медицине';
+});
+Route::get('/tag/{tag}', function (string $tag) {
+    switch ($tag) {
+        case 'политика':
+            return 'новости политики';
+        case 'спорт':
+            return 'новости спорта';
+        case 'авто':
+            return 'новости авто';
+    };
+});
+Route::get('nauka', function () {
+    return 'новости о науке';
+});
+
